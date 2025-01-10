219581fee86ada497900b6c0385cafd63e9070c2
calvulus <introsupr@gmail.com>
2025-01-10 21:49:10 +0100

    primer Commit

---
+++ src/biblioteca/Prestamo.java
@@ -0,0 +1,25 @@
+package biblioteca;
+
+public class Prestamo {
+	private int IdPréstamo;
+	private Libro libro;
+	private Usuario usuario;
+	private int fechaDePréstamo;
+	private int fechaDeDevolución;
+	
+	public crear préstamo()
+	{
+		
+	}
+	
+	public marcar devolución()
+	{
+		
+	}
+	
+	public validar disponibilidad() 
+	{
+		
+	}
+
+}
